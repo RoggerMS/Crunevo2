c9146b726fce56a6c19912c35a109ef23aad6113
RoggerMS <raongta@gmail.com>
2025-06-12 00:17:02 -0500

    Unlock Donador achievement on voluntary transfers

--- tests/test_credits.py
+++ tests/test_credits.py
@@ -1,5 +1,5 @@
 from crunevo.utils.credits import add_credit, spend_credit
-from crunevo.constants import CreditReasons
+from crunevo.constants import CreditReasons, AchievementCodes
 import pytest
 
 
@@ -24,3 +24,9 @@ def test_spend_credit(db_session, test_user):
 def test_spend_insufficient(db_session, test_user):
     with pytest.raises(ValueError):
         spend_credit(test_user, 1, CreditReasons.COMPRA)
+
+
+def test_donor_achievement(db_session, test_user, another_user):
+    add_credit(test_user, 5, CreditReasons.DONACION)
+    spend_credit(test_user, 5, CreditReasons.DONACION, related_id=another_user.id)
+    assert any(a.badge_code == AchievementCodes.DONADOR for a in test_user.achievements)
