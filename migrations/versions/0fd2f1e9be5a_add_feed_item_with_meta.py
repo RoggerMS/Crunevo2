1ffb3a1bda41c9f76cb83fcbbfe01c50993991f2
RoggerMS <raongta@gmail.com>
2025-06-14 23:55:59 -0500

    Update 0fd2f1e9be5a_add_feed_item_with_meta.py

--- migrations/versions/0fd2f1e9be5a_add_feed_item_with_meta.py
+++ migrations/versions/0fd2f1e9be5a_add_feed_item_with_meta.py
@@ -31,7 +31,7 @@ def upgrade():
             """
         )
 
-    # ⛔️ El enum debe estar definido DENTRO de upgrade()
+    #  El enum debe estar definido DENTRO de upgrade()
     item_enum = sa.Enum(
         "apunte",
         "post",
@@ -40,7 +40,7 @@ def upgrade():
         "evento",
         "mensaje",
         name="feed_item_type",
-        create_type=False,  # 👈🏻 este flag evita el error
+        create_type=False,  #  este flag evita el error
     )
 
     op.create_table(
