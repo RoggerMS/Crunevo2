f78da6a4d4464efc2bcb1605cfa72546720ee88b
RoggerMS <raongta@gmail.com>
2025-06-19 18:36:58 -0500

    Update __init__.py

--- crunevo/__init__.py
+++ crunevo/__init__.py
@@ -2,9 +2,13 @@
 
 from .app import create_app as _create_app
 from .routes import main_routes
-
+import os
 
 def create_app():
     app = _create_app()
-    app.register_blueprint(main_routes.main_bp)
+
+    if os.environ.get("ADMIN_INSTANCE") != "1":
+        app.register_blueprint(main_routes.main_bp)
+
     return app
+
