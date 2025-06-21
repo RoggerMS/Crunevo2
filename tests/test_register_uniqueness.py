6ee7d398f59f5b31e48334bf0a12cd76d41cb77d
RoggerMS <raongta@gmail.com>
2025-06-21 02:36:05 -0500

    Unify register route and update template

--- tests/test_register_uniqueness.py
+++ tests/test_register_uniqueness.py
@@ -13,21 +13,3 @@ def test_onboarding_register_duplicate_email(client, db_session):
     )
     assert resp.status_code == 400
     assert User.query.filter_by(email="dup@example.com").count() == 1
-
-
-def test_auth_register_duplicate_username(client, db_session):
-    user = User(username="dupuser", email="dupuser@example.com", activated=True)
-    user.set_password("StrongPassw0rd!")
-    db_session.add(user)
-    db_session.commit()
-
-    resp = client.post(
-        "/register",
-        data={
-            "username": "dupuser",
-            "email": "other@example.com",
-            "password": "StrongPassw0rd!",
-        },
-    )
-    assert resp.status_code == 400
-    assert User.query.filter_by(username="dupuser").count() == 1
