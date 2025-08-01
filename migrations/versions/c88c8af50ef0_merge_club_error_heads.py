217ece4d72182d9f77eef8f55034c4cafddd85c6
RoggerMS <raongta@gmail.com>
2025-08-01 03:20:56 -0500

    Fix migration chain and format

---
+++ migrations/versions/c88c8af50ef0_merge_club_error_heads.py
@@ -0,0 +1,15 @@
+"""Merge club_modernization_2025 and add_system_error_log heads"""
+
+# revision identifiers, used by Alembic.
+revision = "c88c8af50ef0"
+down_revision = ("add_system_error_log", "club_modernization_2025")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
