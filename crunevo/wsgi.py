3ba855ecb06950d6b256e56da664d69c3862ce5a
RoggerMS <raongta@gmail.com>
2025-08-09 01:59:19 -0500

    Update wsgi.py

--- crunevo/wsgi.py
+++ crunevo/wsgi.py
@@ -1,28 +1,4 @@
-from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from crunevo.app import create_app
-from crunevo.extensions import talisman
 
 # Creamos la aplicación principal
-flask_app = create_app()
-
-# Creamos una mini-app súper ligera SOLO para el health check
-health_app = Flask(__name__)
-
-
-@health_app.route("/healthz")
-@talisman(force_https=False)
-def health():
-    """Responde instantáneamente. No toca la base de datos ni la app principal."""
-    return "ok", 200
-
-
-# Usamos un Dispatcher para enrutar el tráfico.
-# Si la petición es para /healthz, va a la app ligera.
-# Para todo lo demás, va a la app principal.
-app = DispatcherMiddleware(
-    flask_app,  # App principal como default
-    {
-        "/healthz": health_app,  # Health check en ruta específica
-    },
-)
+app = create_app()
