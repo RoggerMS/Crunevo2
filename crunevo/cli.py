669579be960c085a004462f05ff2a1aec3ba92b3
RoggerMS <raongta@gmail.com>
2025-08-08 20:25:06 -0500

    ko

---
+++ crunevo/cli.py
@@ -0,0 +1,8 @@
+from crunevo.app import create_app
+
+# Create a Flask app instance specifically for CLI commands
+# This is separate from the WSGI application which uses DispatcherMiddleware
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
